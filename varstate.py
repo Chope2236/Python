fcfb0a8c75557659d9aaee6d16ae0971114b2986
unknown <Chopetrumpis@gmail.com>
2021-08-09 00:43:24 -0300

    Variables and statements

--- varstate.py
+++ varstate.py
@@ -1,11 +1,17 @@
+#Testing data types and statements
+#strings
 name = "Mathew"
 last_name = "Jhonson"
 full_name = name + " " + last_name
+#integers
 age = 43
+#floats
 height = 170.5
+#booleans
 human = False
 print(full_name + " Is " + str(age))
 print("He is " + str(height) + "cm tall")
+#statements
 if human == True:
     print("Is human")
 else:
