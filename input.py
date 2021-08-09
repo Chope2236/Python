5c0cef31ff3d34fe209420b5da0930878a0b2518
unknown <Chopetrumpis@gmail.com>
2021-08-09 14:35:31 -0300

    input testing

---
+++ input.py
@@ -0,0 +1,8 @@
+name = input("Whats your name?: ")
+age = int(input("How old are you?: "))
+year = 2021
+yearborn = year - age
+
+print("Hello " +name)
+print("You are " + str(age) + " years old")
+print("You were born in " + str(yearborn))
