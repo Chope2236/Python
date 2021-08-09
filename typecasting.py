1711de767eb85163534959a879d6164fc242f52f
unknown <Chopetrumpis@gmail.com>
2021-08-09 14:23:12 -0300

    type casting

---
+++ typecasting.py
@@ -0,0 +1,11 @@
+a = 1
+b = 6.5
+c = "10"
+
+a = float(a)
+b = int(b)
+c = int(c)
+
+print(a)
+print(b)
+print(c*3) #you cant do maths with strings so you need to convert it to an integer, c*3 being a string will display just '101010'
