384955c74957fa356420f5b2bdca6e592be3b756
unknown <Chopetrumpis@gmail.com>
2021-08-09 15:00:41 -0300

    math library testing

---
+++ maths.py
@@ -0,0 +1,11 @@
+import math
+
+num = int(input("Write a number: "))
+sqrt = math.sqrt(num)
+a = 1
+b = 2
+c = 3
+print("The square root rounded of your number is " + str(round(sqrt)))
+print("Your number to the power 2 is " + str(math.pow(num,2)))
+print(max(a,b,c))
+print(min(a,b,c))
