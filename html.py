0cf08a9bb9b7a12d2a3d1272e6609e682923b83c
unknown <Chopetrumpis@gmail.com>
2021-08-10 15:42:56 -0300

    added main function and declared url and strtofind as objects

--- html.py
+++ html.py
@@ -2,8 +2,14 @@ from os import write
 import requests
 from requests.models import DEFAULT_REDIRECT_LIMIT
 
+def main():
+ main.url = str(input("Insert a complete URL (https:// etc etc): "))
+ get_request_and_file()
+ main.strtofind = str(input('Insert the string that you want to find: '))
+ find_string_file()
+
 def get_request_and_file():
- r = requests.get(url)
+ r = requests.get(main.url)
  if r.status_code == 200:
     print("GET request executed succesfully.")
     f = open("html.txt", "w")
@@ -14,12 +20,12 @@ def get_request_and_file():
 
 def find_string_file():
  f = open("html.txt", "r")
- if strtofind in f.read():
-     print("String founded.")
+ if main.strtofind in f.read():
+     print("String found.")
  else:
      print("String not found.")
-url = str(input("Insert a complete URL (https:// etc etc): "))
-get_request_and_file()
-strtofind = str(input('Insert the string that you want to find: '))
-find_string_file()
+
+if __name__ == "__main__":
+    main()
+
 
