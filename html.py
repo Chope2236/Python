4a4e71d062a4eb7e0b046f89535b52aba3a06d12
unknown <Chopetrumpis@gmail.com>
2021-08-10 01:20:01 -0300

    created a function to the get request and the file usage

--- html.py
+++ html.py
@@ -1,8 +1,16 @@
 from os import write
 import requests
+from requests.models import DEFAULT_REDIRECT_LIMIT
+
+def get_request_and_file():
+ r = requests.get(url)
+ if r.status_code == 200:
+    print("GET request executed succesfully.")
+    f = open("html.txt", "w")
+    f.write(str(r.text.encode("utf-8")))
+ else:
+    print("GET request failed.")
 
 url = str(input("Insert a complete URL (https:// etc etc): "))
-r = requests.get(url)
-f = open("html.txt", "w")
-f.write(str(r.text.encode("utf-8")))
+get_request_and_file()
 
