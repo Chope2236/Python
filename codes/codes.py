b69486679ab7a7b4d3e94d86654e0e04b5025620
unknown <Chopetrumpis@gmail.com>
2021-12-20 10:27:16 -0300

    Changed the way to scan barcodes and URL's in QRCodes now open automatically

--- codes/codes.py
+++ codes/codes.py
@@ -1,6 +1,11 @@
 import json
+from re import L
+from types import CodeType
+from typing import ItemsView, Text
 from pyzbar import pyzbar
 from PIL import Image
+import webbrowser
+import re
 
 def main():
 
@@ -8,30 +13,36 @@ def main():
      data = json.load(json_file)
      ch = str(input("Insert qrcode to scan a QR code or barcode to scan a Barcode: "))
 
-     if 'qrcode' in ch:
+     def qrcode():
           qrloc = str(input("Insert the QRCode Image location: "))
           qr = Image.open(qrloc)
           output = pyzbar.decode(qr)
-          data = output[0].data
-          print(data)
-          return 0
+          data = str(output[0].data)
+          if 'https://' in data:
+           URL = str(re.search("(?P<url>https?://[^\s]+)", data).group("url"))
+           webbrowser.open(URL)
+          else:
+               textfromqr = str(output[0].data, 'utf-8')
+               print(textfromqr)
+
+     def barcode():
+      bcloc = str(input("Insert the Barcode Image Location:"))
+      bc = Image.open(bcloc)
+      output = pyzbar.decode(bc)
+      barcodenumber = str(output[0].data, "utf-8")
+      print(barcodenumber)
+      for i in data:
+           code = str(i['code'])
+           name = i['name']
+           if barcodenumber in code:
+                print(name)
+     
+
+     if 'qrcode' in ch:     
+      qrcode()
 
      elif 'barcode' in ch:
-          barcode = str(input("Insert the Bar Code (EAN 13) : "))
-     while len(barcode) > 13:
-        print("ERROR, Bar code length cant be higher than 13")
-        barcode = str(input("Insert the Bar Code (EAN 13) : "))
+      barcode()
 
-     for i in data:
-      code = str(i['code'])
-     name = i['name']
-     items = code + " - " + name
-
-     if barcode in code:
-        print(name)
-     elif 'all' in barcode:
-        print(items)
-     
-     return 0
 if __name__ == '__main__':
      main()
