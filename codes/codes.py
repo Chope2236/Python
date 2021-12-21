5fb03abc0461463135e26453bbae02c148a18b6a
unknown <Chopetrumpis@gmail.com>
2021-12-20 22:27:43 -0300

    made a simple UI and combined QRCode and Barcode functions

--- codes/codes.py
+++ codes/codes.py
@@ -1,48 +1,38 @@
+from tkinter import font
+import PIL
 import json
-from re import L
-from types import CodeType
-from typing import ItemsView, Text
 from pyzbar import pyzbar
 from PIL import Image
 import webbrowser
 import re
+from tkinter import *
+from tkinter import filedialog
+window = Tk()
+window.title('QRCode Scaner')
+window.geometry("300x300")
 
 def main():
-
-     json_file = open("C:\\Users\\Chope\\Desktop\\programacion\\Python\\codes\\items.json", "r")
-     data = json.load(json_file)
-     ch = str(input("Insert qrcode to scan a QR code or barcode to scan a Barcode: "))
-
-     def qrcode():
-          qrloc = str(input("Insert the QRCode Image location: "))
-          qr = Image.open(qrloc)
-          output = pyzbar.decode(qr)
-          data = str(output[0].data)
-          if 'https://' in data:
-           URL = str(re.search("(?P<url>https?://[^\s]+)", data).group("url"))
-           webbrowser.open(URL)
-          else:
-               textfromqr = str(output[0].data, 'utf-8')
-               print(textfromqr)
-
-     def barcode():
-      bcloc = str(input("Insert the Barcode Image Location:"))
-      bc = Image.open(bcloc)
-      output = pyzbar.decode(bc)
-      barcodenumber = str(output[0].data, "utf-8")
-      print(barcodenumber)
-      for i in data:
+    json_file = open("C:\\Users\\Chope\\Desktop\\programacion\\Python\\codes\\items.json", "r")
+    data = json.load(json_file)
+    IMG = PIL.Image
+    filename = filedialog.askopenfilename(initialdir = "C:/Users/Desktop",title = "Select a File",filetypes = (("PNG Files","*.png*"),("all files","*.*")))
+    imgtsc = PIL.Image.open(filename)
+    output = pyzbar.decode(imgtsc)
+    datastr = str(output[0].data, 'utf-8')
+    if 'https://' in data:
+        URL = str(re.search("(?P<url>https?://[^\s]+)", data).group("url"))
+        webbrowser.open(URL)
+    else:
+        for i in data:
            code = str(i['code'])
            name = i['name']
-           if barcodenumber in code:
-                print(name)
-     
+           if datastr in code:
+                itemlbl = Label(window, text=name, font=("Helvetica",12), background="red")
+                itemlbl.place(x=10,y=0)
 
-     if 'qrcode' in ch:     
-      qrcode()
+qrcodeimg = PhotoImage(file="C:\\Users\\Chope\\Desktop\\frame2.png")
+button_explore = Button(window, image=qrcodeimg, command= main,bd=0, height=256, width=256,bg="white")
+button_explore.place(x=20,y=10)
 
-     elif 'barcode' in ch:
-      barcode()
-
-if __name__ == '__main__':
-     main()
+window.config(background="white")
+window.mainloop()
