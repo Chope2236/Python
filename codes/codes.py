bfa4424eae194a4ee33df413e945f95bda1efefa
unknown <Chopetrumpis@gmail.com>
2021-12-22 18:29:28 -0300

    Changed functions, added arguments and fixed bugs.

--- codes/codes.py
+++ codes/codes.py
@@ -14,32 +14,35 @@ window.geometry("300x300")
 window.resizable(0,0)
 
 def main():
-    json_item_file()
-    decoding()
-    #looks for URL
-    if 'https://' in decoding.datastr:
-        URL = str(re.search("(?P<url>https?://[^\s]+)", decoding.datastr).group("url"))
+    datastr = decoding()
+    #looks for an URL
+    try:
+        URL = str(re.search("(?P<url>https?://[^\s]+)", datastr).group("url"))
         webbrowser.open(URL)
-    else:
-        #looks for item
-        for i in json_item_file.data:
-           code = str(i['code'])
-           name = i['name']
-           if decoding.datastr in code:
-               itemlbl = Label(window, text=name, font=("Helvetica",8), background="red")
-               itemlbl.place(x=10,y=235)
-               itemlbl.pack()
-               window.after(3000, itemlbl.destroy)
-
-def json_item_file():
-    json_file = open(".\items.json", "r")
-    json_item_file.data = json.load(json_file)
+    except AttributeError:
+        URL = str(re.search("(?P<url>https?://[^\s]+)", datastr))
+    
+    lookforitem(datastr)
 
 def decoding():
     filename = filedialog.askopenfilename(initialdir = "./",title = "Select a File",filetypes = (("PNG Files","*.png*"),("all files","*.*")))
     imgtsc = PIL.Image.open(filename)
     output = pyzbar.decode(imgtsc)
-    decoding.datastr = str(output[0].data, 'utf-8')
+    datastr = str(output[0].data, 'utf-8')
+    return datastr
+
+def lookforitem(datastr):
+    json_file = open(".\items.json", "r")
+    data = json.load(json_file)
+    for i in data:
+           code = str(i['code'])
+           name = i['name']
+           if datastr in code:
+               itemlbl = Label(window, text=name, font=("Helvetica",8), background="red")
+               itemlbl.place(x=10,y=235)
+               itemlbl.pack()
+               window.after(3000, itemlbl.destroy)
+               return 0
 
 qrcodeimg = PhotoImage(file=".\\images\\btn_image.png")
 button_explore = Button(window, image=qrcodeimg, command= main,bd=0, height=256, width=256,bg="white")
